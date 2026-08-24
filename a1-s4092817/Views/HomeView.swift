9b1e6067fcbe17182be055a53cb45e8b4ca2e094
s4092817 <s4092817@student.rmit.edu.au>
2026-08-25 01:22:23 +1000

    feat: created new file for HomeView

---
+++ a1-s4092817/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  a1-s4092817
+//
+//  Created by Riley Tran on 25/8/2026.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
