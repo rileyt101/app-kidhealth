c75536f956f4b28773f56453fc67e86f82317cb7
s4092817 <s4092817@student.rmit.edu.au>
2026-08-27 17:23:40 +1000

    feat: add allergy creattion view file

---
+++ a1-s4092817/Views/AllergyCreateView.swift
@@ -0,0 +1,18 @@
+//
+//  AllergyCreateView.swift
+//  a1-s4092817
+//
+//  Created by Riley Tran on 27/8/2026.
+//
+
+import SwiftUI
+
+struct AllergyCreateView: View {
+    var body: some View {
+        Text("AllergyCreateView")
+    }
+}
+
+#Preview {
+    AllergyCreateView()
+}
