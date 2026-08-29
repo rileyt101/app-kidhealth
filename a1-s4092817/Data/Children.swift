fd89c1505532eb04685e2fb6d661d844f913be66
s4092817 <s4092817@student.rmit.edu.au>
2026-08-30 01:19:07 +1000

    fix: minor changes for hard coded data

--- a1-s4092817/Data/Children.swift
+++ a1-s4092817/Data/Children.swift
@@ -9,8 +9,8 @@ let childrenData: [Child] = [
     Child(name: "Ryan"
          ),
     Child(name: "Amanda",
-          allergies: [Allergy(name: "Eggs", severity: "Severe", medication: "EpiPen"),
-                      Allergy(name: "Peanuts", severity: "Strong", medication: "EpiPen")]
+          allergies: [Allergy(name: "Eggs", severity: "Severe", medication: "EpiPen", notes: "EpiPen is kept in the front pocket of her schoolbag. Allergic to the whites of the egg."),
+                      Allergy(name: "Peanuts", severity: "Strong", medication: "EpiPen", notes: "EpiPen is kept in the front pocket of her schoolbag.")]
          ),
     Child(name: "Sammy",
           allergies: [Allergy(name: "Walnuts", severity: "Severe", medication: "EpiPen"),
