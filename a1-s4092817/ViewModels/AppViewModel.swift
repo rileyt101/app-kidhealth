ce13afb87f039fc717af0a603335cbf119b3feb5
s4092817 <s4092817@student.rmit.edu.au>
2026-08-29 23:01:41 +1000

    feat: implemented input field variables in view model

--- a1-s4092817/ViewModels/AppViewModel.swift
+++ a1-s4092817/ViewModels/AppViewModel.swift
@@ -15,6 +15,14 @@ final class AppViewModel: ObservableObject {
     // MARK: - States
     @Published private(set) var children: [Child] = childrenData
     
+    // MARK: - Allergy entry fields
+    @Published var nameField: String = ""
+    @Published var severityField: String = ""
+    @Published var medicationField: String = ""
+    @Published var notesField: String = ""
+    
+    @Published var currentChild: Child?
+    
     // MARK: - Derived values
     var alphabeticallyOrderedChildren: [Child] {
         let sorted = children.sorted { $0.name < $1.name }
@@ -29,5 +37,31 @@ final class AppViewModel: ObservableObject {
     // Used for Preview testing
     func getChildByName(name: String) -> Child {
         return children.first(where: { $0.name == name })!
+    }
+    
+    func addAllergyEntry(child: Child) -> Bool {
+        guard let childIndex = children.firstIndex(where: { $0.id == child.id }) else {
+            return false
+        }
+        
+        if (nameField.isEmpty) {
+            return false
+        }
+        else {
+            let newAllergy = Allergy(name: nameField,
+                                     severity: severityField,
+                                     medication: medicationField,
+                                     notes: notesField)
+            children[childIndex].allergies = (children[childIndex].allergies ?? []) + [newAllergy]
+            return true
+        }  
+    }
+    
+    // MARK: - Functions for Allergy Creation page logic
+    func resetInputFields() {
+        nameField = ""
+        severityField = ""
+        medicationField = ""
+        nameField = ""
     }
 }
